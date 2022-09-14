2a023c7d0fa9a0251153300e067bf9f1551910e8
DongukHam <hdw8607@gmail.com>
2022-09-14 13:02:47 +0900

    update

--- challenge_Day9_final/js/join-login-button.js
+++ challenge_Day9_final/js/join-login-button.js
@@ -1,1 +1,10 @@
 const LoginJoin = document.querySelector("#login-join");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function buttonHandler(event) {
+    event.preventDefault();
+    LoginJoin.classList.add(HIDDEN_CLASSNAME);
+};
+
+LoginJoin.addEventListener("submit", buttonHandler);
